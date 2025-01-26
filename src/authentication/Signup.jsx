a7f169b4db54154efd8561f6c4210e314b04263b
zakaryae rouane <rouanezakaria052@gmail.com>
2025-01-26 20:00:24 +0100

    update authentication pages add regix for confirm all inputs before submit

--- src/authentication/Signup.jsx
+++ src/authentication/Signup.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 
 export default function Register() {
   const [fullname, setfullname] = useState("");
@@ -11,9 +10,79 @@ export default function Register() {
   const [password, setpassword] = useState("");
   const [confirmpassword, setconfirmpassword] = useState("");
 
+  const [valid, setvalid] = useState(false);
+  const [error, seterror] = useState("");
+
+  const Submit = (e) => {
+    e.preventDefault();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex = /^[A-Z][a-z]{4,}[@#$!%*?&][0-9]{1,}$/;
+  
+    if (
+      fullname === "" ||
+      email === "" ||
+      role === "" ||
+      gender === "" ||
+      cities === "" ||
+      birthyear === "" ||
+      password === "" ||
+      confirmpassword === ""
+    ) {
+      setvalid(true);
+      seterror("Please fill all the fields");
+    } else if (!emailRegex.test(email)) {
+      setvalid(true);
+      seterror("Please enter a valid email address");
+    } else if (!passwordRegex.test(password)) {
+      setvalid(true);
+      seterror(
+        "Password must start with a capital letter, have 4+ lowercase letters, include a special character (e.g., @), a number, and be 8+ characters long"
+      );
+    } else if (password !== confirmpassword) {
+      setvalid(true);
+      seterror("Passwords do not match");
+    } else {
+      setvalid(false);
+      seterror("");
+      window.location.href = "/pages/codeconfirm";
+    }
+  };
   return (
     <div className="Register">
       <h1 id="h1">Create Account</h1>
+      <form action="" method="post">
+      {valid && (
+          <div className="error">
+            <div className="error__icon">
+              <svg
+                fill="none"
+                height="24"
+                viewBox="0 0 24 24"
+                width="24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="m13 13h-2v-6h2zm0 4h-2v-2h2zm-1-15c-1.3132 0-2.61358.25866-3.82683.7612-1.21326.50255-2.31565 1.23915-3.24424 2.16773-1.87536 1.87537-2.92893 4.41891-2.92893 7.07107 0 2.6522 1.05357 5.1957 2.92893 7.0711.92859.9286 2.03098 1.6651 3.24424 2.1677 1.21325.5025 2.51363.7612 3.82683.7612 2.6522 0 5.1957-1.0536 7.0711-2.9289 1.8753-1.8754 2.9289-4.4189 2.9289-7.0711 0-1.3132-.2587-2.61358-.7612-3.82683-.5026-1.21326-1.2391-2.31565-2.1677-3.24424-.9286-.92858-2.031-1.66518-3.2443-2.16773-1.2132-.50254-2.5136-.7612-3.8268-.7612z"
+                  fill="#393a37"
+                ></path>
+              </svg>
+            </div>
+            <div className="error__title">{error}</div>
+            {/* <div className="error__close">
+              <svg
+                height="20"
+                viewBox="0 0 20 20"
+                width="20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="m15.8333 5.34166-1.175-1.175-4.6583 4.65834-4.65833-4.65834-1.175 1.175 4.65833 4.65834-4.65833 4.6583 1.175 1.175 4.65833-4.6583 4.6583 4.6583 1.175-1.175-4.6583-4.6583z"
+                  fill="#393a37"
+                ></path>
+              </svg>
+            </div> */}
+          </div>
+        )}
       <input
         type="text"
         name="fullname"
@@ -138,9 +207,8 @@ export default function Register() {
         onChange={(e) => setconfirmpassword(e.target.value)}
       />
       <br />
-      <Link to="/pages/codeconfirm">
-        <button id="btnRegister">Register</button>
-      </Link>
+        <button id="btnRegister" onClick={Submit}>Register</button>
+      </form>
     </div>
   );
 }
