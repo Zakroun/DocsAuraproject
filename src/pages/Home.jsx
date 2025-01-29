e6c4c02b5f02adb4a7dbb7ca23786fd255ccbdf8
zakaryae rouane <rouanezakaria052@gmail.com>
2025-01-29 07:42:41 +0100

    add services and button to move up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import Header from "../static/header";
 import Footer from "../static/Footer";
 import Divimage2 from "../static/dibimage2";
+import DivService1 from "../static/divservice1";
+import { GoMoveToTop } from "react-icons/go";
+import Services from "../static/Services";
 export default function Home() {
     return(<>
         <Header></Header>
         <Divimage2></Divimage2>
+        <DivService1></DivService1>
+        <button className="moveToTop" onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}><GoMoveToTop></GoMoveToTop></button>
+        <Services></Services>
         <Footer></Footer>
         </>
     )
--- src/static/divservice1.jsx
+++ src/static/divservice1.jsx
@@ -1,3 +1,25 @@
 import { FaMicroscope } from "react-icons/fa6";
 import { FaClinicMedical } from "react-icons/fa";
 import { FaUserDoctor } from "react-icons/fa6";
+
+export default function DivService1() {
+    return(
+        <div className="services1">
+            <div className="service">
+                <FaUserDoctor size={40} className="iconbox"/>
+                <h3>Doctors</h3>
+                <p>Our medical services are designed to provide accurate and timely results to help you make informed decisions about your health.</p>
+            </div>
+            <div className="service">
+                <FaMicroscope size={40} className="iconbox"/>
+                <h3>LABORATORIES</h3>
+                <p>Our medical services are designed to provide accurate and timely results to help you make informed decisions about your health.</p>
+            </div>
+            <div className="service">
+                <FaClinicMedical size={40} className="iconbox"/>
+                <h3>CLINICS</h3>
+                <p>Our medical services are designed to provide accurate and timely results to help you make informed decisions about your health.</p>
+            </div>
+        </div>
+    )
+}
