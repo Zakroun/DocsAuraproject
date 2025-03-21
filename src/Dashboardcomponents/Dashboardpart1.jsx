e761ed60e865dd3a1c51b5596b7cb4c521b03f5e
zakaryae rouane <rouanezakaria052@gmail.com>
2025-03-21 15:31:09 +0000

    add menu

--- src/Dashboardcomponents/Dashboardpart1.jsx
+++ src/Dashboardcomponents/Dashboardpart1.jsx
@@ -7,49 +7,143 @@ import { RiLoginBoxFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { changeboard } from "../data/DocsauraSlice";
-
+import { IoMenu } from "react-icons/io5";
+import { useState } from "react";
 export default function Part1Dashboard(props) {
   const user = props.Use;
   const dispatch = useDispatch();
+  const [menuOpen, setMenuOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
-    <div className="Part1Dashboard">
-      <div className="Part1Dashboard__header">
-        <Link to={"/"}>
-          <img
-            src="/Images/Asset 10.png"
-            alt="logodashboard"
-            className="logodashboard"
+    <>
+      <div className="Part1Dashboard_menu">
+        <div className="Part1Dashboard__header">
+          <IoMenu className="menu-icon" size={50} onClick={toggleMenu} />
+        </div>
+        <div
+          className={`Part1Dashboard_active ${menuOpen ? "open" : "closed"}`}
+        >
+          <div className="content">
+            <IoMenu className="menu-icon2" size={50} onClick={toggleMenu} />
+            <IoMdHome
+              className="icon enabled"
+              onClick={() => dispatch(changeboard("home"))}
+            />
+            <FaCalendarAlt
+              className={`icon ${
+                user.Verified === false ? "disabled" : "enabled"
+              }`}
+              onClick={
+                user.Verified === false
+                  ? null
+                  : () => dispatch(changeboard("calander"))
+              }
+            />
+            <BiSolidMessageSquareDetail
+              className={`icon ${
+                user.Verified === false ? "disabled" : "enabled"
+              }`}
+              onClick={
+                user.Verified === false
+                  ? null
+                  : () => dispatch(changeboard("messages"))
+              }
+            />
+            <IoMdSettings
+              className={`icon ${
+                user.Verified === false ? "disabled" : "enabled"
+              }`}
+              onClick={
+                user.Verified === false
+                  ? null
+                  : () => dispatch(changeboard("settings"))
+              }
+            />
+            <GoFileSubmodule
+              className={`icon ${
+                user.Verified === false ? "disabled" : "enabled"
+              }`}
+              onClick={
+                user.Verified === false
+                  ? null
+                  : () => dispatch(changeboard("files"))
+              }
+            />
+          </div>
+          <div className="footerdashbord">
+            <RiLoginBoxFill
+              className="iconleft"
+              onClick={() => dispatch(changeboard("Logout"))}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="Part1Dashboard">
+        <div className="Part1Dashboard__header">
+          <Link to={"/"}>
+            <img
+              src="/Images/Asset 10.png"
+              alt="logodashboard"
+              className="logodashboard"
+            />
+          </Link>
+        </div>
+        <div className="content">
+          <IoMdHome
+            className="icon enabled"
+            onClick={() => dispatch(changeboard("home"))}
           />
-        </Link>
+          <FaCalendarAlt
+            className={`icon ${
+              user.Verified === false ? "disabled" : "enabled"
+            }`}
+            onClick={
+              user.Verified === false
+                ? null
+                : () => dispatch(changeboard("calander"))
+            }
+          />
+          <BiSolidMessageSquareDetail
+            className={`icon ${
+              user.Verified === false ? "disabled" : "enabled"
+            }`}
+            onClick={
+              user.Verified === false
+                ? null
+                : () => dispatch(changeboard("messages"))
+            }
+          />
+          <IoMdSettings
+            className={`icon ${
+              user.Verified === false ? "disabled" : "enabled"
+            }`}
+            onClick={
+              user.Verified === false
+                ? null
+                : () => dispatch(changeboard("settings"))
+            }
+          />
+          <GoFileSubmodule
+            className={`icon ${
+              user.Verified === false ? "disabled" : "enabled"
+            }`}
+            onClick={
+              user.Verified === false
+                ? null
+                : () => dispatch(changeboard("files"))
+            }
+          />
+        </div>
+        <div className="footerdashbord">
+          <RiLoginBoxFill
+            className="iconleft"
+            onClick={() => dispatch(changeboard("Logout"))}
+          />
+        </div>
       </div>
-      <div className="content">
-        <IoMdHome
-          className="icon enabled"
-          onClick={() => dispatch(changeboard("home"))}
-        />
-        <FaCalendarAlt
-          className={`icon ${user.Verified === false ? "disabled" : "enabled"}`}
-          onClick={user.Verified === false ? null : () => dispatch(changeboard("calander"))}
-        />
-        <BiSolidMessageSquareDetail
-          className={`icon ${user.Verified === false ? "disabled" : "enabled"}`}
-          onClick={user.Verified === false ? null : () => dispatch(changeboard("messages"))}
-        />
-        <IoMdSettings
-          className={`icon ${user.Verified === false ? "disabled" : "enabled"}`}
-          onClick={user.Verified === false ? null : () => dispatch(changeboard("settings"))}
-        />
-        <GoFileSubmodule
-          className={`icon ${user.Verified === false ? "disabled" : "enabled"}`}
-          onClick={user.Verified === false ? null : () => dispatch(changeboard("files"))}
-        />
-      </div>
-      <div className="footerdashbord">
-        <RiLoginBoxFill className="iconleft" 
-        onClick={()=>dispatch(changeboard("Logout"))}
-        />
-      </div>
-    </div>
+    </>
   );
 }
